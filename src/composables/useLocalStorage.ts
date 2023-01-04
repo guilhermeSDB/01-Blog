e335beddbbd5a0aa837550351f5946cacfbb52b5
Guilherme S <guilherme_pikapal@hotmail.com>
2023-01-03 23:05:02 -0300

    Fix: Removido console log

--- src/composables/useLocalStorage.ts
+++ src/composables/useLocalStorage.ts
@@ -4,12 +4,10 @@ import { LocalStorageEnum } from '../types/LocalStorageEnum';
 export function setLocalStorage(key: LocalStorageEnum, value: Array<Article>): void{
     const valueStringify = JSON.stringify(value);
     localStorage.setItem(key, valueStringify);
-    console.log('Salvo no Local Storage com Sucesso', value);
 }
 
 export function getLocalStorage(key: LocalStorageEnum){
     const value = localStorage.getItem(key);
     const articles: Array<Article> = value ? JSON.parse(value) : [];
-    console.log('Recuperado do Local Storage com Sucesso', articles);
     return articles
 }
